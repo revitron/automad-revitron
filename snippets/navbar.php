f6c6f11361440c732a25c6a1f5d69c7d44421f1a
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-09-09 11:32:48 +0200

    Added alternative logo for small devices

--- snippets/navbar.php
+++ snippets/navbar.php
@@ -1,8 +1,8 @@
 <nav class="wy-nav-top" aria-label="top navigation">
 	<i data-toggle="wy-nav-top" class="fa fa-bars"></i>
 	<a href="/">
-		<@ if @{ imageLogo } @>
-			<img src="@{ imageLogo }" class="logo" alt="Logo"/>
+		<@ if @{ imageLogoMobile | def (@{ imageLogo }) } @>
+			<img src="@{ imageLogoMobile | def (@{ imageLogo }) }" class="logo" alt="Logo"/>
 		<@ else @>
 			@{ brand | def (@{ sitename }) }
 		<@ end @>	
