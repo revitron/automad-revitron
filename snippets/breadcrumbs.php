de8bb67a6bb5b2cf86fe981220984a851652e39b
Marc Anton Dahmen <mail@marcdahmen.de>
2020-11-14 22:41:31 +0100

    Fixed breadcrumbs

--- snippets/breadcrumbs.php
+++ snippets/breadcrumbs.php
@@ -1,6 +1,5 @@
 <div role="navigation" aria-label="breadcrumbs navigation">
 	<ul class="wy-breadcrumbs">
-		<li><a href="/">@{ sitename }</a><@ if @{ url } != '/' @> ⁄&nbsp;<@ end @></li>
 		<@ newPagelist { type: 'breadcrumbs' } @>
 		<@ foreach in pagelist @>
 			<li><a href="@{ url }">@{ title }</a><@ if not @{ :current } @> ⁄&nbsp;<@ end @></li>
