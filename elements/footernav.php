7b6177c23de2e34cad7d5663a58eee7f0c49d7c3
Marc Anton Dahmen <mail@marcdahmen.de>
2021-05-18 18:42:21 +0200

    Fixed matching of unchecked checkboxes

--- elements/footernav.php
+++ elements/footernav.php
@@ -5,7 +5,7 @@
 </div> 
 <nav class="nav-footer">
 	<# @{ checkboxShowInFooter } #>
-	<@ newPagelist { excludeHidden: false, match: '{"checkboxShowInFooter": "/1/"}' } @>
+	<@ newPagelist { excludeHidden: false, match: '{"checkboxShowInFooter": "/[^0]+/"}' } @>
 	<ul>
 		<@ foreach in pagelist @>
 			<li><a href="@{ url }">@{ title }</a></li>
