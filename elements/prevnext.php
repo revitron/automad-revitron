c3dfb5dc9e95ad27d3051f5b9d2ef3191931ec94
Marc Anton Dahmen <mail@marcdahmen.de>
2025-09-15 19:16:09 +0200

    fix: fix prev-next navigation title attributes

--- elements/prevnext.php
+++ elements/prevnext.php
@@ -2,12 +2,12 @@
 	<@ newPagelist { type: 'siblings' } @>
 	<div class="rst-footer-buttons" role="navigation" aria-label="footer navigation">
 		<@ with next @>
-			<a href="@{ url }" class="btn btn-neutral float-right" title="Changelog" accesskey="n" rel="next">
+			<a href="@{ url }" class="btn btn-neutral float-right" title="@{ title }" accesskey="n" rel="next">
 				@{ title } &nbsp;<b>⟶</b>
 			</a>
 		<@ end @>
 		<@ with prev @>
-			<a href="@{ url }" class="btn btn-neutral float-left" title="Configuration" accesskey="p" rel="prev">
+			<a href="@{ url }" class="btn btn-neutral float-left" title="@{ title }" accesskey="p" rel="prev">
 				<b>⟵</b>&nbsp; @{ title }
 			</a>
 		<@ end @>
